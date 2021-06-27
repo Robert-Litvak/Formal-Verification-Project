423ff10c462b715cdb6861e4cddf3950bc6b04cc
Robert Litvak <robert1393@gmail.com>
2021-06-28 02:56:40 +0300

    Added three more verified benchmarks

--- benchmarks/c/other.c
+++ benchmarks/c/other.c
@@ -23,7 +23,7 @@ int mc91(int n){
 	return n;
 }
 
-int square(int x){
+int tricky_square(int x){
 	int result = 0;
 	int dif = 1;
 	for (int i = 0; i < x; i += 1){
@@ -33,7 +33,7 @@ int square(int x){
 	return result;
 }
 
-int foo(int x, int iterations){
+int tricky_sub(int x, int iterations){
 	int odd = 1;
 	int even = 2;
 	int result = x;
@@ -44,4 +44,32 @@ int foo(int x, int iterations){
 		even += 2;
 	}
 	return result;
+}
+
+void two(int x){
+	int temp = x*3;
+	for (int i = 1; i <= 6; i += 1){
+		temp += 1;
+	}
+	int magic = (temp/3) - x;
+}
+
+void three_same_digits(int d){
+	int number = 0;
+	int digits_sum = 0;
+	for (int i = 1; i < 1000; i *= 10){
+		number += d * i;
+		digits_sum += d;
+	}
+}
+
+int three_digit_number_duplication(int x){
+	int y = 0;
+	int z = 0;
+	for (int i = 1; i < 1000000; i *= 1000){
+		y += x * i;
+	}
+	z = x * 7;
+	z = z * 11;
+	z = z * 13;
 }
